b02008095e568f4c0c8d66c5caf00d8ba1aa093a
jduga002 <jduga002>
2014-07-23 12:37:35 -0700

    QuizData.hs now has more Constructors for the QuizData Data type. Main now includes a new route that reads in a file, converts them into QuizData, and displays them to the webpage.

--- src/Main.hs
+++ src/Main.hs
@@ -8,14 +8,16 @@ import           Snap.Snaplet.Session.Backends.CookieSession
 import qualified Data.ByteString.Char8 as B
 import           App
 import           Root
+import           QuizData
 
 
 --Initializes the app to use Session Snaplet
-appInit :: B.ByteString -> B.ByteString -> B.ByteString -> SnapletInit App App
-appInit header footer about = makeSnaplet "myapp" "Sample Page" Nothing $ do
+appInit :: B.ByteString -> B.ByteString -> B.ByteString -> String -> SnapletInit App App
+appInit header footer about quizdata = makeSnaplet "myapp" "Sample Page" Nothing $ do
     ss <- nestSnaplet "session" sess $ initCookieSessionManager "session.txt" "_session" (Just 3600)
     addRoutes [ ("/", sessionHandler header footer)
               , ("about", writeBS about)
+              , ("quizdata", writeBS $ B.pack quizdata)
               ]
     return $ App ss
 
@@ -26,4 +28,5 @@ main = do
     header <- B.readFile "./src/header.html"
     footer <- B.readFile "./src/footer.html"
     about <- B.readFile "./src/about.html"
-    serveSnaplet defaultConfig $ appInit header footer about
+    quizdata <- readit "./src/quizdata.txt"
+    serveSnaplet defaultConfig $ appInit header footer about quizdata
--- src/QuizData.hs
+++ src/QuizData.hs
@@ -13,28 +13,47 @@ data QuizData = Soda { getName :: String
               | Person { getName :: String
                        , isGood :: BoolM
                        , getPicURL :: String
-                       } deriving (Read, Show, Eq)
+                       }
+              | Airline { getName :: String
+                        , getPicURL :: String
+                        } 
+              | PoltIssue { getName :: String
+                          , getPicURL :: String
+                          } deriving (Read, Show, Eq)
                      
 parseQuizData :: String -> Maybe QuizData
 parseQuizData xs
-              | xs == []                    = Nothing
-              | head (words xs) == "Soda"   = parseSoda endxs
-              | head (words xs) == "Person" = parsePerson endxs
-              | otherwise                   = Nothing
-                where endxs = tail $ words $ xs
+              | xs == []             = Nothing
+              | first == "Soda"      = parseSoda endxs
+              | first == "Person"    = parsePerson endxs
+              | first == "Airline"   = parseAirline endxs
+              | first == "PoltIssue" = parsePoltIssue endxs
+              | otherwise            = Nothing
+                where endxs = tail $ words xs
+                      first = head $ words xs
               
               
 parseSoda :: [String] -> Maybe QuizData
 parseSoda xs
           | length xs /= 2 = Nothing
-          | otherwise      = Just $ Soda (head xs) (last xs)
+          | otherwise      = Just $ Soda (headwSpace xs) (last xs)
           
 parsePerson :: [String] -> Maybe QuizData
 parsePerson xs
-            | length xs == 2 = Just $ Person (head xs) (Maybe) (last xs)
-            | length xs == 3 = Just $ Person (head xs) (f xs)  (last xs)
+            | length xs == 2 = Just $ Person (headwSpace xs) (Maybe) (last xs)
+            | length xs == 3 = Just $ Person (headwSpace xs) (f xs)  (last xs)
             | otherwise      = Nothing
               where f = read . head . tail
+              
+parseAirline :: [String] -> Maybe QuizData
+parseAirline xs
+             | length xs /= 2 = Nothing
+             | otherwise      = Just $ Airline (headwSpace xs) (last xs)
+              
+parsePoltIssue :: [String] -> Maybe QuizData
+parsePoltIssue xs
+               | length xs /= 2 = Nothing
+               | otherwise      = Just $ PoltIssue (headwSpace xs) (last xs)
               
 readit :: String -> IO String
 readit filename = do
@@ -44,6 +63,18 @@ readit filename = do
 parseFile :: [String] -> String
 parseFile [] = ""
 parseFile (x:xs)
-    | parseQuizData x == Nothing = "Not valid quiz data type\n" ++ (parseFile xs)
-    | otherwise                  = (show q) ++ "\n" ++ (parseFile xs)
+    | parseQuizData x == Nothing = "Not valid quiz data type<br>" ++ (parseFile xs)
+    | otherwise                  = (show q) ++ "<br>" ++ (parseFile xs)
       where Just q = parseQuizData x
+
+headwSpace :: [String] -> String
+headwSpace xs = map underscoreToSpace $ head xs 
+
+underscoreToSpace :: Char -> Char
+underscoreToSpace '_' = ' '
+underscoreToSpace c = c
+      
+{-main :: IO ()
+main = do
+    quizdata <- readit "quizdata.txt"
+    putStr quizdata-}
