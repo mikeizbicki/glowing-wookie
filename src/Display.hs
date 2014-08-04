f831294ddc6a2e5b0738d6c5ecc52c16ca147b00
jduga002 <jduga002>
2014-08-04 13:02:37 -0700

    Edits to get images to display on webpage, dynamically add routes

--- src/Display.hs
+++ src/Display.hs
@@ -1,27 +1,27 @@
 module Display
 	where
 
-import System.IO
-import Data.Char
-import QuizData
+import           System.IO
+import           Data.Char
+import           QuizData
 
 						 
-display :: QuizData -> QuizData -> String
-display q1 q2 = "<html>\n\n"
+display :: Question -> String
+display q = "<html>\n\n"
 			 ++ "<head>\n"
 			 ++ "<LINK REL=StyleSheet HREF=\"css/default.css\" TYPE=\"text/css\" MEDIA=screen>\n"
 			 ++ "</head>\n\n"
 			 ++ "<body>\n\n"
 			 ++ "<h1>Which is better?</h1>\n"
 			 ++ "<div class=\"container\">\n"
-			 ++ displayonething q1
+			 ++ displayonething (fst q)
 			 ++ "<div class=\"middle\">or</div>\n"
-			 ++ displayonething q2
+			 ++ displayonething (snd q)
 			 ++ "</div>\n\n"
 			 ++ "</body>\n\n"
 			 ++ "</html>\n\n"
 			 
 			 
 displayonething :: QuizData -> String
-displayonething x = "<a href=\"ex3.html\"><div class=\"queryobject\"><img src=\"img/" 
-				 ++ getPicURL x ++ "\"><p>" ++ getName x ++ "</div></a>\n"
+displayonething x = "<a href=\"ex3.html\"><div class=\"queryobject\"><img src=\"/img/" 
+				 ++ getPicURL x ++ "\"><p>" ++ getName x ++ "</p></div></a>\n"
