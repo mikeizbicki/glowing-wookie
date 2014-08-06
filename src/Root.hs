7a685fdc3c72b57c5b5e37938fe00f7c92194f8e
Dat <dat@dat-GS70.(none)>
2014-08-06 13:41:28 -0700

    Prints CSRF Token, Referer, and URL to csv file now. All debug information has been consolidated to one function

--- src/Root.hs
+++ src/Root.hs
@@ -13,6 +13,7 @@ import           Data.Time.Clock (getCurrentTime)
 import           Data.Time.Format
 import           Web.UAParser
 
+
 --Handles GET and POST requests on / page
 sessionHandler :: B.ByteString -> B.ByteString -> Handler App App ()
 sessionHandler header footer = method GET getter <|> method POST setter
@@ -20,30 +21,24 @@ sessionHandler header footer = method GET getter <|> method POST setter
         getter = do
             sessionList <- with sess $ sessionToList
             writeBS header
-            mapM_ (writeText . snd) sessionList --prints contents of cookie data
 
-            writeText "</br>Date: "
-            printTime
-            
-            --prints number of posts from cookie data
-            writeText "</br>Posts: "
-            let d = lookup "counter" sessionList
-            case d of
-                Just d -> writeText d
-                Nothing -> withSession sess . withTop sess $ setInSession "counter" "0"
+            ref <- getReferer
+            let r = lookup "referer" sessionList
+            case r of
+                Just r -> return ()
+                Nothing -> withSession sess . withTop sess $ setInSession "referer" "\\"
+            let r = lookup "referer" sessionList
+            if r == Just "\\"
+                then withSession sess . withTop sess $ setInSession "referer" ref
+                else return ()
 
-            writeText "</br>IP Address: "
-            printIPAddr
+            sessionList <- with sess $ sessionToList
 
-            writeText "</br>"
-            printUserHeader
-
-            writeText "</br>"
-            getBrowser
-            writeText "</br>"
-            getOS
+            --prints all debug info on webpage
+            printDebugInfo sessionList
 
             writeBS footer
+
         setter = do
             mvalue <- getParam "Question"
             withSession sess . withTop sess $ setInSession ("Question") (convert mvalue)
@@ -51,13 +46,16 @@ sessionHandler header footer = method GET getter <|> method POST setter
             let c = lookup "counter" sessionList
             case c of
                 Just c ->  withSession sess . withTop sess $ setInSession "counter" $ addOne c 
-                Nothing -> writeText "Error: counter not found"
-
-            writeData  $ convertStr mvalue
-
+                Nothing -> withSession sess . withTop sess $ setInSession "counter" "1"
+            let ref = lookup "referer" sessionList
+            --Gets CSRF token from session data
+            csrf <- with sess $ csrfToken
+            --Writes user data to csv file
+            writeData (convertStr mvalue) (show csrf) (show $ fromMaybe "" ref)
             getter
-        convert = T.pack . B.unpack . (fromMaybe "set-error")
-        convertStr = show . B.unpack . fromJust
+
+        convert = T.pack . B.unpack . fromMaybe "set-error"
+        convertStr = show . B.unpack . fromMaybe ""
 
 
 --Used to increment visitor counter
@@ -68,7 +66,7 @@ addOne x = T.pack $ show $ convert x + 1
 
 printIPAddr :: Handler App App ()
 printIPAddr =  do
-            req <- getRequest -- :: Request -> Snap ByteString
+            req <- getRequest
             writeBS (rqRemoteAddr req)
 
 
@@ -91,26 +89,63 @@ getTime = fmap show getCurrentTime
 printUserHeader :: Handler App App ()
 printUserHeader =  do
             reqHeader <- getRequest -- :: Request -> Snap ByteString
-            writeBS (fromJust (getHeader "User-Agent" reqHeader))
+            writeBS $ fromJust $ getHeader "User-Agent" reqHeader
+
+
+getUserHeader :: Handler App App String
+getUserHeader = do
+            req <- getRequest
+            return $ show $ fromJust $ getHeader "User-Agent" req
 
 
 getBrowser :: Handler App App String
 getBrowser = do
         req <- getRequest
-        return $ show $ uarFamily $ fromJust $ parseUA $ fromJust (getHeader "User-Agent" req)
+        return $ show $ uarFamily $ fromJust $ parseUA $ fromJust $ getHeader "User-Agent" req
 
 
 getOS :: Handler App App String
 getOS = do
         req <- getRequest
-        return $ show $ osrFamily $ fromJust $ parseOS $ fromJust (getHeader "User-Agent" req)
+        return $ show $ osrFamily $ fromJust $ parseOS $ fromJust $ getHeader "User-Agent" req
 
 
-writeData :: String -> Handler App App ()
-writeData answer = do 
+getURL :: Handler App App String
+getURL = do
+        req <- getRequest
+        return $ show $ fromJust $ getHeader "Referer" req
+
+
+getReferer :: Handler App App T.Text
+getReferer = do
+        req <- getRequest
+        return $ T.pack $ B.unpack $ fromMaybe "" $ getHeader "Referer" req
+
+
+printDebugInfo :: [(T.Text,T.Text)] -> Handler App App ()
+printDebugInfo sessionList = do
+            mapM_ (writeText . snd) sessionList --prints contents of cookie data
+            writeText "<br>Date: "
+            printTime
+            writeText "<br>IP Address: "
+            printIPAddr
+            writeText "<br>"
+            printUserHeader
+            writeText "<br>"
+            csrf <- with sess $ csrfToken
+            writeText csrf
+            writeText "<br>Posts: "
+            let d = lookup "counter" sessionList
+            writeText $ fromMaybe "0" d
+
+
+writeData :: String -> String -> String -> Handler App App ()
+writeData answer csrf ref = do 
             time <- liftIO $ getTime
             ip <- getIPAddr
+            ua <- getUserHeader
             os <- getOS
             browser <- getBrowser
-            liftIO . appendFile "data.csv" $ time ++ "," ++ ip ++ "," ++ os ++ "," ++ browser ++ "," ++ answer ++ "\n"
+            url <- getURL
+            liftIO . appendFile "data.csv" $ time ++ "," ++ ip ++ "," ++ csrf ++ "," ++ ref ++ "," ++ ua ++ ","  ++ os ++ "," ++ browser ++ "," ++ url ++ "," ++ answer ++ "\n"
 
