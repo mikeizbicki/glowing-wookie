ade6c2071bfe42dc65b90ef95bf7c02da3b2f2e3
holidaylvr <pstar001@ucr.edu>
2014-08-08 11:55:58 -0700

    Incorporated css file into main. Fixed the html in Display to allow pushing of pictures. Included css file for Display to read

--- src/Display.hs
+++ src/Display.hs
@@ -1,73 +1,44 @@
-module Web
+module Display
 	where
 
-import System.IO
-import Data.Char
+import           System.IO
+import           Data.Char
+import           QuizData
 
-data QuizObject = QuizObject { url :: String
-							 , description :: String
-							 } deriving (Show, Read)
 						 
-display :: QuizObject -> QuizObject -> String
-display q1 q2 = "<html>\n\n"
+display :: Question -> String
+display q = "<html>\n\n"
 			 ++ "<head>\n"
-			 ++ "<LINK REL=StyleSheet HREF=\"css/default.css\" TYPE=\"text/css\" MEDIA=screen>\n"
+			 ++ "<link type=\"text/css\" rel=\"stylesheet\" href=\"/css/stylesheet.css\" >\n"
 			 ++ "</head>\n\n"
-			 ++ "<body>\n\n"
-			 ++ "<h1>Which is better?</h1>\n"
-			 ++ "<div class=\"container\">\n"
-			 ++ displayonething q1
-			 ++ "<div class=\"middle\">or</div>\n"
-			 ++ displayonething q2
+			 ++ "<body>\n"
+			 ++ "<div id=\"header\">\n\n"
+			 ++ "<h2>Which is better?</h2>\n"
 			 ++ "</div>\n\n"
-			 ++ "</body>\n\n"
-			 ++ "</html>\n\n"
+			 ++ "<div class=\"container\">\n"
+			 ++ "<div class=\"queryobject\">\n"
+			 ++ displayonething (fst q)
+			 ++ "</div>\n\n"
+			 ++ "<div class=\"middle\">\n"
+			 ++ "<strong style=\"font-size: 100px\">or</strong>\n"
+			 ++ "</div>\n\n"
+			 ++ "<div class=\"queryobject\">\n"
+			 ++ displayonething (snd q)
+			 ++ "</div>\n"
+			 ++ "**Hover over image for description\n"
+			 ++ "</div>\n\n\n"
+			 ++ "<div id=\"footer\">\n"
+			 ++ "</div>\n\n"
+			 ++ "</html>\n"
+			 ++ "</body>\n"
 			 
 			 
-displayonething :: QuizObject -> String
-displayonething x = "<a href=\"ex3.html\"><div class=\"queryobject\"><img src=\"img/" 
-				 ++ url x ++ "\"><p>" ++ description x ++ "</div></a>\n"
-
-{-
-main = do
---display function
-
-	url <- hGetLine "textDocument.txt"
-	description <- hGetLine "textDocument.txt"
-	url2 <- hGetLine "textDocument.txt"
-	description2 <- hGetLine "textDocument.txt"
-	
-	
-	let str = "<html>"
-           ++ "<head>"
-	
-	"<html>"
-	"\n"
-	+"<head>"
-	"<LINK REL=StyleSheet HREF=\"css/default.css\" TYPE=\"text/css\" MEDIA=screen>"
-	"</head>\n"
-	"<body>\n"
-	"<h1>Which is better?</h1>\n"
-	"<div class=\"container\">"
-	"<a href=\"exe.html\"><div class=\"queryobject\"><img src=\"img/" ++ Show url ++ "\"><p>" ++ Show description ++ "</div></a>"
-	"<div class=\"middle\">or</div>"
-	"<a href=\"exe.html\"><div class=\"queryobject\"><img src=\"img/" ++ Show url2 ++  "\"><p>" ++ Show description2 ++ "</div></a>"
-	"</div>\n"
-	"</body>\n"
-	"</html>"
-
-
+displayonething :: QuizData -> String
+{-displayonething x = "<a href=\"ex3.html\"><div class=\"queryobject\"><img src=\"/img/" 
+				 ++ getPicURL x ++ "\"><p>" ++ getName x ++ "</p></div></a>\n"
 -}
-
-	--create data type object with Read / Show abilities
-	
-	--disp :: Foo -> Foo -> String
-	--disp f1 f2 = "<html><body><b>"++show f1++"</b><i>"++show f2"</i></body>"
-
-	--read in from file
-	--disp function creates giant string that is basically the entire webpage
-	--USER (not you) then decides what to do with said string
-	
-	
-	
-
+displayonething x = "<form name=\"input\" action=\"demo_form_action.asp\" method=\"push\">\n"
+				 ++ "<p><strong>" ++ getName x ++ "</strong></p>\n"
+				 ++ "<input type=\"image\" src=\"/img/" ++ getPicURL x ++ "\" id=\"imgWidth\">\n"
+				 ++ "</form>\n"
+				 
